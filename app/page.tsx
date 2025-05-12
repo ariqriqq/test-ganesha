95074b9874e3645f3bd233c9f060b99ed10ba22d
Ariq <jalanpari2@gmail.com>
2025-05-12 11:51:45 +0700

    delete comment

--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,6 @@ import ClientPokemonList from "./pokemon-list";
 import { fetchPokemonList } from "@/api/pokemon-api";
 
 export default async function PokemonPage() {
-  // const res = await fetch(
-  //   "https://pokeapi.co/api/v2/pokemon?offset=0&limit=10"
-  // );
-  // const data = await res.json();
-  // console.log(data);
-
   const data = await fetchPokemonList(0, 10);
 
   return <ClientPokemonList initialData={data} />;
